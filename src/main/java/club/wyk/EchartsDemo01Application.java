6c2d9e3958b7fa921fcb50dcf241c28a69cb81bf
wyk <376129110@qq.com>
2019-12-24 10:34:30 +0800

    Initial commit

---
+++ src/main/java/club/wyk/EchartsDemo01Application.java
@@ -0,0 +1,13 @@
+package club.wyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchartsDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchartsDemo01Application.class, args);
+    }
+
+}
---
+++ src/test/java/club/wyk/EchartsDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package club.wyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchartsDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
